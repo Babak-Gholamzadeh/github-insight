ca2b1d8d63616888df0e027916316cca85d5b762
Babak Gholamzadeh <babak.gholamzadeh92@gmail.com>
2024-02-25 01:12:27 +0100

    Fixed timeline position in gantt-chart

--- src/components/GanttChart/GanttChart.component.jsx
+++ src/components/GanttChart/GanttChart.component.jsx
@@ -31,9 +31,19 @@ const GanttChart = ({ records }) => {
     return [_x, _y, _w, _h];
   };
 
-  const drawImgWithRadius = (ctx, imgUrl, x, y, width, height, radius) => {
+  const drawRectWithRadius = (ctx, x, y, w, h, r, bgColor, strockColor) => {
+    ctx.fillStyle = bgColor;
+    ctx.strokeStyle = strockColor || bgColor;
+    ctx.beginPath();
+    ctx.roundRect(x, y, w, h, r);
+    ctx.stroke();
+    ctx.fill();
+  };
+
+  const drawImgWithRadius = (ctx, imgUrl, x, y, width, height, radius, cb) => {
     const img = new Image();
     img.onload = () => {
+      ctx.globalCompositeOperation = 'destination-over';
       ctx.save();
       ////////
       ctx.beginPath();
@@ -57,6 +67,8 @@ const GanttChart = ({ records }) => {
         height,
       );
       ctx.restore();
+      cb();
+      ctx.globalCompositeOperation = 'source-over';
     };
     img.src = imgUrl;
   };
@@ -77,6 +89,8 @@ const GanttChart = ({ records }) => {
 
     const ctx = canvas.getContext('2d');
     ctx.scale(zoomFactorX, zoomFactorY);
+
+    document.addEventListener('mouseup', onMouseDrag);
 
     const ctxWidth = canvas.width / zoomFactorX;
     const ctxHeight = canvas.height / zoomFactorY;
@@ -107,12 +121,11 @@ const GanttChart = ({ records }) => {
     const TRACK_HEIGHT = 40;
     const TRACK_PADDING_TOP = 5;
     const TRACK_PADDING_BOTTOM = 5;
-    const CELL_MARGIN_RIGHT = 1;
-    const CELL_MARGIN_LEFT = 1;
+    const CELL_MARGIN_RIGHT = 1 / zoomFactorX;
+    const CELL_MARGIN_LEFT = 1 / zoomFactorX;
     const CELL_PADDING_RIGHT = 5;
     const CELL_PADDING_LEFT = 5;
     const TOTAL_TRACKS = Math.ceil((-coordinateOriginYWithZoom + ctxHeight - TIMELINE_HEIGHT) / TRACK_HEIGHT);
-
     // Draw track lines
     const trackOccupancy = {
       tracks: Array.from({ length: TOTAL_TRACKS }, () => -1),
@@ -123,9 +136,12 @@ const GanttChart = ({ records }) => {
         this.tracks[t] = x;
       },
     };
-    for (let i = 0; i < TOTAL_TRACKS; i++) {
-      const [x1, y1] = coor(-coordinateOriginXWithZoom + 0, TIMELINE_HEIGHT + TRACK_HEIGHT * i);
-      const [x2, y2] = coor(-coordinateOriginXWithZoom + ctxWidth, TIMELINE_HEIGHT + TRACK_HEIGHT * i);
+    const trackLineIndex = Math.ceil(-coordinateOriginYWithZoom / TRACK_HEIGHT);
+    const totalVisibleTracks = TOTAL_TRACKS - trackLineIndex;
+    console.log('TOTAL_TRACKS:', TOTAL_TRACKS, 'trackLineIndex:', trackLineIndex, 'totalVisibleTracks:', totalVisibleTracks);
+    for (let i = 0, j = trackLineIndex; i < totalVisibleTracks; i++, j++) {
+      const [x1, y1] = coor(-coordinateOriginXWithZoom + 0, TIMELINE_HEIGHT + TRACK_HEIGHT * j);
+      const [x2, y2] = coor(-coordinateOriginXWithZoom + ctxWidth, TIMELINE_HEIGHT + TRACK_HEIGHT * j);
       // drawLine(x1, y1, x2, y2, '#1b1f26', 1 / zoomFactorY);
       drawLine(ctx, x1, y1, x2, y2, '#333', 1 / zoomFactorY);
     }
@@ -133,15 +149,14 @@ const GanttChart = ({ records }) => {
     // Draw PRs
     const now = Date.now();
     records.forEach(pr => {
-      ctx.strokeStyle = colors[pr.state];
-      ctx.fillStyle = colors[pr.state];
       const closedAtTime = new Date(pr.closed_at).getTime() || now;
       const createdAtTime = new Date(pr.created_at).getTime();
       const x = (now - closedAtTime) * canvas.width / TIME_RANGE;
       const w = (closedAtTime - createdAtTime) * canvas.width / TIME_RANGE;
       const t = trackOccupancy.findEmptyTrack(x);
       if (t < 0)
-        return; // Sorry no empty track to draw
+        return; // Sorry free track to draw
+      trackOccupancy.registerInTrack(t, x + w);
       const y = TIMELINE_HEIGHT + (TRACK_HEIGHT * t + TRACK_PADDING_BOTTOM);
       const h = TRACK_HEIGHT - TRACK_PADDING_BOTTOM - TRACK_PADDING_TOP;
 
@@ -151,22 +166,21 @@ const GanttChart = ({ records }) => {
         w - (CELL_MARGIN_RIGHT + CELL_MARGIN_LEFT),
         h,
       );
-      // Draw Rect
-      ctx.beginPath();
-      ctx.roundRect(nx, ny, nw, nh, 5);
-      ctx.stroke();
-      ctx.fill();
-      // Draw Text
-      const fontSize = 14;
-      ctx.font = `${fontSize}px Segoe UI`;
-      ctx.fillStyle = "white";
-      const charWidth = 7;
-      ctx.fillText(
-        getReadableTimePeriodShorter(pr.longRunning),//.slice(0, (-nw - (CELL_PADDING_LEFT - CELL_PADDING_RIGHT)) / charWidth),
-        // `${pr.longRunning} - ${(new Date(pr.closed_at).getTime() || now) - new Date(pr.created_at).getTime()}`,//.slice(0, (-nw - (CELL_PADDING_LEFT - CELL_PADDING_RIGHT)) / charWidth),
-        nx + nw + CELL_PADDING_RIGHT,
-        ny + (nh / 2) + 5,
-      );
+      const drawTheRest = () => {
+        // Draw Text
+        const fontSize = 14;
+        ctx.font = `${fontSize}px Segoe UI`;
+        ctx.fillStyle = "white";
+        const charWidth = 7;
+        ctx.fillText(
+          getReadableTimePeriodShorter(pr.longRunning),//.slice(0, (-nw - (CELL_PADDING_LEFT - CELL_PADDING_RIGHT)) / charWidth),
+          // `${pr.longRunning} - ${(new Date(pr.closed_at).getTime() || now) - new Date(pr.created_at).getTime()}`,//.slice(0, (-nw - (CELL_PADDING_LEFT - CELL_PADDING_RIGHT)) / charWidth),
+          nx + nw + CELL_PADDING_RIGHT,
+          ny + (nh / 2) + 5,
+        );
+        // Draw Rect
+        drawRectWithRadius(ctx, nx, ny, nw, nh, 5, colors[pr.state]);
+      };
       // Draw Image
       const imgWidth = 20;
       const imgHeight = 20;
@@ -178,24 +192,40 @@ const GanttChart = ({ records }) => {
         imgWidth,
         imgHeight,
         imgWidth / 2,
+        drawTheRest,
       );
-      trackOccupancy.registerInTrack(t, x + w);
     });
-
     // Draw Timeline
-    const [x1, y1] = coor(-coordinateOriginXWithZoom + 0, TIMELINE_HEIGHT / 2);
-    const [x2, y2] = coor(-coordinateOriginXWithZoom + ctxWidth, TIMELINE_HEIGHT / 2);
-    drawLine(ctx, x1, y1, x2, y2, 'red', 1 / zoomFactorY);
+    const fixedX = -coordinateOriginXWithZoom + 0;
+    const fixedW = -coordinateOriginXWithZoom + ctxWidth;
+    const fixedY = -coordinateOriginYWithZoom + 0;
+    {
+      const [x, y, w, h] = coor(fixedX, fixedY, ctxWidth, TIMELINE_HEIGHT);
+      // @TODO: Add gradient color
+      drawRectWithRadius(ctx, x, y, w, h, 0, '#0d1117');
+    }
+    {
+      const [x1, y1] = coor(fixedX, fixedY + TIMELINE_HEIGHT / 2);
+      const [x2, y2] = coor(fixedW, fixedY + TIMELINE_HEIGHT / 2);
+      drawLine(ctx, x1, y1, x2, y2, 'red', 1 / zoomFactorY);
+    }
     const monthRangeWidth = TIME_RANGE / TOTAL_MONTHS * canvas.width / TIME_RANGE;
-    for (let i = 0; i < (-coordinateOriginXWithZoom + ctxWidth) / monthRangeWidth; i++) {
-      const p = monthRangeWidth * i;
+    const totalSection = ctxWidth / monthRangeWidth + 1;
+    const shortLineIndex = Math.floor(-coordinateOriginXWithZoom / monthRangeWidth);
+    for (let i = 0, j = shortLineIndex; i < totalSection; i++, j++) {
+      const p = monthRangeWidth * j;
       const lineLength = 5 / zoomFactorY;
-      const [x1, y1] = coor(p, TIMELINE_HEIGHT / 2 - lineLength);
-      const [x2, y2] = coor(p, TIMELINE_HEIGHT / 2 + lineLength);
+      const [x1, y1] = coor(p, fixedY + TIMELINE_HEIGHT / 2 - lineLength);
+      const [x2, y2] = coor(p, fixedY + TIMELINE_HEIGHT / 2 + lineLength);
       // console.log([x1, y1], [x2, y2]);
       drawLine(ctx, x1, y1, x2, y2, 'yellow', 1 / zoomFactorX);
     }
 
+
+    return () => {
+      document.removeEventListener('mouseup', onMouseDrag);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [
     records,
     zoomFactorX,
@@ -232,17 +262,23 @@ const GanttChart = ({ records }) => {
 
   const onWheel = e => {
     const zoomDelta = Math.sign(e.deltaY) * -.1;
+    const bounds = e.target.getBoundingClientRect();
     if (isShiftPressed) {
+      const cursorX = -zoomDelta * (refCanvas.current.width - (e.clientX - bounds.left) - coordinateOriginX);
+      setCoordinateOriginX(coordinateOriginX + cursorX);
       const newZoomFactorX = Math.max(zoomFactorX + zoomFactorX * zoomDelta, .001);
       setZoomFactorX(newZoomFactorX);
     }
     if (isAltPressed) {
+      // const cursorY = -zoomDelta * (refCanvas.current.height - (e.clientY - bounds.top) - coordinateOriginY);
+      // setCoordinateOriginY(coordinateOriginY + cursorY);
       const newZoomFactorY = Math.max(zoomFactorY + zoomFactorY * zoomDelta, .001);
       setZoomFactorY(newZoomFactorY);
     }
   };
 
   const onMouseDrag = e => {
+    // console.log('e.type:', e.type, 'isDragging:', isDragging);
     // eslint-disable-next-line default-case
     switch (e.type) {
       case 'mousedown':
@@ -257,9 +293,9 @@ const GanttChart = ({ records }) => {
         if (isDragging) {
           const x = -(e.clientX - startDragPosX);
           const y = -(e.clientY - startDragPosY);
-          console.log('x: %d, y: %d', x, y);
-          console.log('coorX: %d, coorY: %d', coordinateOriginX, coordinateOriginY);
-          const newCoorX = Math.min(coordinateOriginX + x, 0);
+          // console.log('x: %d, y: %d', x, y);
+          // console.log('coorX: %d, coorY: %d', coordinateOriginX, coordinateOriginY);
+          const newCoorX = coordinateOriginX + x;
           const newCoorY = Math.min(coordinateOriginY + y, 0);
           setCoordinateOriginX(newCoorX);
           setCoordinateOriginY(newCoorY);
@@ -282,7 +318,6 @@ const GanttChart = ({ records }) => {
         onWheel={onWheel}
         onMouseDown={onMouseDrag}
         onMouseMove={onMouseDrag}
-        onMouseUp={onMouseDrag}
       />
     </div>
   );
